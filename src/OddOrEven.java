af11307442daa714a19eb98e066d32b8dbe3d49f
Gangothri <gangothrigadige985@gmail.com>
2022-06-27 21:39:13 +0530

    implemented method

--- src/OddOrEven.java
+++ src/OddOrEven.java
@@ -1,2 +1,13 @@
 public class OddOrEven {
+    public static void checkOddOrEven(int number){
+        if(number % 2==0){
+            System.out.println(number+" is even");
+        }else{
+            System.out.println(number+" is odd");
+        }
+    }
+
+    public static void main(String[] args) {
+        checkOddOrEven(5);
+    }
 }
